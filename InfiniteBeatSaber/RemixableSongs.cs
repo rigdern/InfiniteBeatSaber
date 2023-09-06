73c595cb956fb9df50d0caeae8e23400da7ef6fd
Adam Comella <rigdern@users.noreply.github.com>
2023-09-05 18:57:11 -0700

    Add support for song Beat Saber by Jaroslav Beck

--- InfiniteBeatSaber/RemixableSongs.cs
+++ InfiniteBeatSaber/RemixableSongs.cs
@@ -6,8 +6,12 @@ namespace InfiniteBeatSaber
     {
         private static readonly Dictionary<string, string> _levelIdToAnalysisPath = new Dictionary<string, string>
         {
-            // Gangnam Style by PSY. Mapped by GreatYazer.
+            // Gangnam Style by PSY. Mapped by GreatYazer. https://bsaber.com/songs/141/
             { "custom_level_8E7E553099436AF31564ADF1977A5EC42A61CFFF", "SpotifyAnalyses.custom_level_8E7E553099436AF31564ADF1977A5EC42A61CFFF.json" },
+            
+            // Beat Saber by Jaroslav Beck.
+            { "custom_level_B68BF61AC6BE0E128BE32A85810D42E7C53F4756", "SpotifyAnalyses.custom_level_B68BF61AC6BE0E128BE32A85810D42E7C53F4756.json" },
+
         };
 
         public static bool IsDifficultyBeatmapRemixable(IDifficultyBeatmap difficultyBeatmap)
