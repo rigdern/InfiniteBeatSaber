302f4b0247dbe39504d4a21d5437bda3e83a7686
Adam Comella <rigdern@users.noreply.github.com>
2023-09-08 17:35:21 -0700

    Remove dependency on BSML since we were only using 1 small function from it

--- InfiniteBeatSaber/InfiniteBeatSaberMenuUI.cs
+++ InfiniteBeatSaber/InfiniteBeatSaberMenuUI.cs
@@ -1,6 +1,6 @@
-﻿using BeatSaberMarkupLanguage;
-using InfiniteBeatSaber.Patches;
+﻿using InfiniteBeatSaber.Patches;
 using IPA.Utilities;
+using TMPro;
 using UnityEngine.Events;
 using UnityEngine.UI;
 using Zenject;
@@ -13,18 +13,15 @@ namespace InfiniteBeatSaber
 
         private readonly StandardLevelDetailViewController _standardLevelDetailViewController;
         private readonly SoloFreePlayFlowCoordinator _soloFreePlayFlowCoordinator;
-        private readonly LevelCollectionViewController _levelCollectionViewController;
 
         private Button _startInfiniteBeatSaberButton;
 
         public InfiniteBeatSaberMenuUI(
             StandardLevelDetailViewController standardLevelDetailViewController,
-            SoloFreePlayFlowCoordinator soloFreePlayFlowCoordinator,
-            LevelCollectionViewController levelCollectionViewController)
+            SoloFreePlayFlowCoordinator soloFreePlayFlowCoordinator)
         {
             _standardLevelDetailViewController = standardLevelDetailViewController;
             _soloFreePlayFlowCoordinator = soloFreePlayFlowCoordinator;
-            _levelCollectionViewController = levelCollectionViewController;
         }
 
         public void Initialize()
@@ -76,7 +73,7 @@ namespace InfiniteBeatSaber
         {
             var detailView = _standardLevelDetailViewController.GetField<StandardLevelDetailView, StandardLevelDetailViewController>("_standardLevelDetailView");
             var startInfiniteBeatSaberButton = UnityEngine.Object.Instantiate(detailView.practiceButton, detailView.practiceButton.transform.parent);
-            BeatSaberUI.SetButtonText(startInfiniteBeatSaberButton, label);
+            SetButtonText(startInfiniteBeatSaberButton, label);
             startInfiniteBeatSaberButton.onClick.RemoveAllListeners();
             startInfiniteBeatSaberButton.onClick.AddListener(onClick);
             return startInfiniteBeatSaberButton;
@@ -87,6 +84,44 @@ namespace InfiniteBeatSaber
             var practiceViewController = _soloFreePlayFlowCoordinator.GetField<PracticeViewController, SinglePlayerLevelSelectionFlowCoordinator>("_practiceViewController");
             practiceViewController.practiceSettings?.ResetToDefault();
             _soloFreePlayFlowCoordinator.InvokeMethod<object, SinglePlayerLevelSelectionFlowCoordinator>("HandlePracticeViewControllerDidPressPlayButton");
+        }
+
+        // `SetButtonText` is derived from BeatSaberMarkupLanguage (https://github.com/monkeymanboy/BeatSaberMarkupLanguage).
+        //
+        // MIT License
+        //
+        // Copyright (c) 2019 David L
+        //
+        // Permission is hereby granted, free of charge, to any person obtaining a copy
+        // of this software and associated documentation files (the "Software"), to deal
+        // in the Software without restriction, including without limitation the rights
+        // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+        // copies of the Software, and to permit persons to whom the Software is
+        // furnished to do so, subject to the following conditions:
+        //
+        // The above copyright notice and this permission notice shall be included in all
+        // copies or substantial portions of the Software.
+        //
+        // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+        // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+        // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+        // AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+        // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+        // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+        // SOFTWARE.
+        public static void SetButtonText(Button button, string text)
+        {
+            Polyglot.LocalizedTextMeshProUGUI localizer = button.GetComponentInChildren<Polyglot.LocalizedTextMeshProUGUI>(true);
+            if (localizer != null)
+            {
+                UnityEngine.Object.Destroy(localizer);
+            }
+
+            TextMeshProUGUI tmpUgui = button.GetComponentInChildren<TextMeshProUGUI>();
+            if (tmpUgui != null)
+            {
+                tmpUgui.SetText(text);
+            }
         }
 
         #endregion
